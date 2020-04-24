7e5703ddfda140911e87351da85b0bb05a2e4e0b
Julieth <juliethquinchia@gmail.com>
2020-04-24 14:51:22 -0500

    Updates

---
+++ precache-manifest.ae43dabec34133ce31b45f3a9d3cca43.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34bd375dd22680e5b9bf3a14a1f70651",
+    "url": "/Prueba-Tecnica/index.html"
+  },
+  {
+    "revision": "a14612c72f12e14eee52",
+    "url": "/Prueba-Tecnica/static/css/2.81dddadc.chunk.css"
+  },
+  {
+    "revision": "f6d6704f3de23974d5c7",
+    "url": "/Prueba-Tecnica/static/css/main.739c88dd.chunk.css"
+  },
+  {
+    "revision": "a14612c72f12e14eee52",
+    "url": "/Prueba-Tecnica/static/js/2.46a673a2.chunk.js"
+  },
+  {
+    "revision": "14060d5403b5cb599a71704f7b391b06",
+    "url": "/Prueba-Tecnica/static/js/2.46a673a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6d6704f3de23974d5c7",
+    "url": "/Prueba-Tecnica/static/js/main.73139543.chunk.js"
+  },
+  {
+    "revision": "a5ed6e0edfac0adc89cb",
+    "url": "/Prueba-Tecnica/static/js/runtime-main.3ec3e41b.js"
+  },
+  {
+    "revision": "c627e23ded21b6d7b4e35e8ddf99d920",
+    "url": "/Prueba-Tecnica/static/media/icons8-github-512.c627e23d.png"
+  }
+]);
